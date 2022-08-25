95dc0fe19d1d162dec1abafe983b8bd7b3ad21a1
Igor Alekseev <i.alekseev@iac.spb.ru>
2022-08-25 18:02:08 +0300

    Update

--- src/main/java/ru/example/backend/ticket/TicketApplication.java
+++ src/main/java/ru/example/backend/ticket/TicketApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketApplication {
 
     public static void main(String[] args) {
-        String[] myArgs = new String[args.length + 1];
-        myArgs[myArgs.length - 1] = " --spring.config.location=config/local/local.yml";
-        SpringApplication.run(TicketApplication.class, myArgs);
+        SpringApplication.run(TicketApplication.class, args);
     }
 
 }
